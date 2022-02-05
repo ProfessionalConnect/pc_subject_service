278e514b2f7212320e28878e6345645f84f82dff
koreaaerospaceU <zx811@naver.com>
2022-02-06 00:00:14 +0900

    feat: Subject Controller 로직 구현

---
+++ src/main/kotlin/com/pro/subject/controller/SubjectController.kt
@@ -0,0 +1,31 @@
+package com.pro.subject.controller
+
+import com.pro.subject.dto.SubjectRequest
+import com.pro.subject.dto.SubjectResponse
+import com.pro.subject.service.SubjectService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+/**
+ * Created by Minky on 2022-02-05
+ */
+
+@RestController
+@RequestMapping("/api/v1/subjects")
+class SubjectController {
+    @Autowired
+    private lateinit var subjectService: SubjectService
+
+    @GetMapping("/{teamId}")
+    fun getSubjectsByTeamId(@PathVariable teamId: Long): ResponseEntity<List<SubjectResponse>> {
+        return ResponseEntity.ok(subjectService.getSubjectsByTeamId(teamId))
+    }
+
+    @PostMapping
+    fun setSubject(@RequestBody subjectRequest: SubjectRequest): ResponseEntity<Void> {
+        val subjectId = subjectService.setSubject(subjectRequest)
+        return ResponseEntity.created(URI("/api/v1/subjects/${subjectId}")).build()
+    }
+}
