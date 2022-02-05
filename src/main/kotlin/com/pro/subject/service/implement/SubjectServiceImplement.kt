f6593e6429135d600af15ed07118400faaf1cf5c
koreaaerospaceU <zx811@naver.com>
2022-02-06 00:55:33 +0900

    feat: Subject Service 로직 추가 - 테스트 케이스 배치 삽입 로직 추가 - 서비스 로직 함수 형태 변경

--- src/main/kotlin/com/pro/subject/service/implement/SubjectServiceImplement.kt
+++ src/main/kotlin/com/pro/subject/service/implement/SubjectServiceImplement.kt
@@ -1,8 +1,10 @@
 package com.pro.subject.service.implement
 
+import com.pro.subject.domain.TestCase
 import com.pro.subject.dto.SubjectRequest
 import com.pro.subject.dto.SubjectResponse
 import com.pro.subject.repository.SubjectRepository
+import com.pro.subject.repository.TestCaseRepository
 import com.pro.subject.service.SubjectService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -15,14 +17,22 @@ import org.springframework.stereotype.Service
 class SubjectServiceImplement: SubjectService {
     @Autowired
     private lateinit var subjectRepository: SubjectRepository
+    @Autowired
+    private lateinit var testCaseRepository: TestCaseRepository
 
-    override fun getSubjectsByTeamId(teamId: Long): List<SubjectResponse> {
-        return SubjectResponse.listOf(subjectRepository.findByTeamId(teamId))
-    }
+    override fun getSubjectsByTeamId(teamId: Long) = SubjectResponse.listOf(subjectRepository.findByTeamId(teamId))
 
     override fun setSubject(subjectRequest: SubjectRequest): Long? {
         // TODO(사용자에 따른 팀 인증)
         val subject = subjectRepository.save(subjectRequest.toEntity())
+        var testCaseList = mutableListOf<TestCase>()
+
+        for (testArgument in subjectRequest.testArguments) {
+            testCaseList.add(TestCase(subject, testArgument))
+        }
+
+        testCaseRepository.saveAll(testCaseList)
+
         return subject.id
     }
 }
