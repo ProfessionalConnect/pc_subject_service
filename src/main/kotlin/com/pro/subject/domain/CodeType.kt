7612c793dbfae5bde2767dcfdd224e4e7856a33c
koreaaerospaceU <zx811@naver.com>
2022-02-05 23:58:46 +0900

    feat: Subject 도메인 제작 - 과제 도메인 제작 - 과제 코드 타입 Enum 클래스 제작

---
+++ src/main/kotlin/com/pro/subject/domain/CodeType.kt
@@ -0,0 +1,11 @@
+package com.pro.subject.domain
+
+/**
+ * Created by Minky on 2022-02-05
+ */
+enum class CodeType {
+    JAVA,
+    C,
+    C_PLUS_PLUS,
+    PYTHON,
+}
---
+++ src/main/kotlin/com/pro/subject/domain/Subject.kt
@@ -0,0 +1,43 @@
+package com.pro.subject.domain
+
+import javax.persistence.*
+
+/**
+ * Created by Minky on 2022-02-05
+ */
+
+@Entity
+@Table(name = "subject_tbl")
+class Subject(
+    teamId: Long,
+    uuid: String,
+    title: String,
+    description: String,
+    correctCode: String,
+    codeType: CodeType,
+): BaseTimeEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subject_sequence_gen")
+    @SequenceGenerator(name = "subject_sequence_gen", sequenceName = "subject_sequence")
+    @Column(name = "subject_id", nullable = false)
+    var id: Long? = null
+
+    @Column(nullable = false)
+    var teamId = teamId
+
+    @Column(nullable = false)
+    var uuid = uuid
+
+    @Column(nullable = false)
+    var title = title
+
+    @Column(nullable = false)
+    var description = description
+
+    @Column(nullable = false)
+    var correctCode = correctCode
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    var codeType = codeType
+}
