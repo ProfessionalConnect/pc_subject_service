ec235fedc3ae3512318286bbbafaa01b70939e3a
koreaaerospaceU <zx811@naver.com>
2022-02-07 00:25:43 +0900

    feat: Grade Repository 검색 메소드 추가

--- src/main/kotlin/com/pro/subject/repository/GradeRepository.kt
+++ src/main/kotlin/com/pro/subject/repository/GradeRepository.kt
@@ -1,10 +1,17 @@
 package com.pro.subject.repository
 
 import com.pro.subject.domain.Grade
+import com.pro.subject.domain.Subject
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 /**
  * Created by Minky on 2022-02-06
  */
+
+@Repository
 interface GradeRepository: JpaRepository<Grade, Long> {
+    fun findBySubject(subject: Subject): List<Grade>
+    fun findBySubjectAndUuid(subject: Subject, uuid: String): List<Grade>
+    fun findByUuid(uuid: String): List<Grade>
 }
