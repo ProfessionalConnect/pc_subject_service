99d064498891de97583203e6635bbe0c43a3cf8d
koreaaerospaceU <zx811@naver.com>
2022-02-07 00:27:49 +0900

    feat: Grade Controller 로직 추가 - 특정 과제 제출 현황 조회 - Pro 특정 과제 제출 전체 현황 조회 - 특정 과제 조회 - 전체 과제 제출 현환 조회

--- src/main/kotlin/com/pro/subject/controller/SubjectController.kt
+++ src/main/kotlin/com/pro/subject/controller/SubjectController.kt
@@ -1,9 +1,6 @@
 package com.pro.subject.controller
 
-import com.pro.subject.dto.ExecResponse
-import com.pro.subject.dto.GradeRequest
-import com.pro.subject.dto.SubjectRequest
-import com.pro.subject.dto.SubjectResponse
+import com.pro.subject.dto.*
 import com.pro.subject.service.SubjectService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -20,17 +17,50 @@ class SubjectController {
     @Autowired
     private lateinit var subjectService: SubjectService
 
-    @GetMapping("/{teamId}")
-    fun getSubjectsByTeamId(@PathVariable teamId: Long): ResponseEntity<List<SubjectResponse>> =
+    @GetMapping("/teams/{teamId}")
+    fun getSubjectsByTeamId(
+        @PathVariable teamId: Long,
+    ): ResponseEntity<List<SubjectResponse>> =
         ResponseEntity.ok(subjectService.getSubjectsByTeamId(teamId))
 
+    @GetMapping("/{subjectId}")
+    fun getSubject(
+        @PathVariable subjectId: Long,
+    ): ResponseEntity<SubjectResponse> =
+        ResponseEntity.ok(subjectService.getSubject(subjectId))
+
     @PostMapping
-    fun setSubject(@RequestBody subjectRequest: SubjectRequest): ResponseEntity<Void> {
-        val subjectId = subjectService.setSubject(subjectRequest)
+    fun setSubject(
+        @RequestBody subjectRequest: SubjectRequest,
+        @RequestHeader(value = "uuid") uuid: String
+    ): ResponseEntity<Void> {
+        val subjectId = subjectService.setSubject(uuid, subjectRequest)
         return ResponseEntity.created(URI("/api/v1/subjects/${subjectId}")).build()
     }
 
     @PostMapping("/register")
-    fun setGrade(@RequestBody gradeRequest: GradeRequest): ResponseEntity<ExecResponse> =
-        ResponseEntity.ok(subjectService.setGrade(gradeRequest))
+    fun setGrade(
+        @RequestBody gradeRequest: GradeRequest,
+        @RequestHeader(value = "uuid") uuid: String
+    ): ResponseEntity<ExecResponse> =
+        ResponseEntity.ok(subjectService.setGrade(uuid, gradeRequest))
+
+    @GetMapping("/{subjectId}/grades")
+    fun getGradeBySubjectAndUUID(
+        @RequestHeader(value = "uuid") uuid: String,
+        @PathVariable subjectId: Long
+    ): ResponseEntity<List<GradeResponse>> =
+        ResponseEntity.ok(subjectService.getGradeBySubjectAndUUID(uuid, subjectId))
+
+    @GetMapping("/{subjectId}/grades/master")
+    fun getGradeBySubject(
+        @PathVariable subjectId: Long
+    ): ResponseEntity<List<GradeResponse>> =
+        ResponseEntity.ok(subjectService.getGradeBySubject(subjectId))
+
+    @GetMapping("/grades")
+    fun getGradeByUUID(
+        @RequestHeader(value = "uuid") uuid: String
+    ): ResponseEntity<List<GradeResponse>> =
+        ResponseEntity.ok(subjectService.getGradeByUUID(uuid))
 }
