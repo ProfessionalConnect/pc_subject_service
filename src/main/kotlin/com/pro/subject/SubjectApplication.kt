8408c394a1b7c2fcd78354339f09277086e99d75
koreaaerospaceU <zx811@naver.com>
2022-02-11 00:01:49 +0900

    feat: Feign Client logging 설정 추가

--- src/main/kotlin/com/pro/subject/SubjectApplication.kt
+++ src/main/kotlin/com/pro/subject/SubjectApplication.kt
@@ -1,9 +1,11 @@
 package com.pro.subject
 
+import feign.Logger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -12,4 +14,7 @@ class SubjectApplication
 
 fun main(args: Array<String>) {
     runApplication<SubjectApplication>(*args)
+
+    @Bean
+    fun feignLoggerLevel() = Logger.Level.FULL
 }
